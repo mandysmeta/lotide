d1bf1ee057ca373bfaf9ba302106cd1852229da7
Miranda Miller <mirandaamiller13@gmail.com>
2022-01-12 15:59:00 +0000

    without.js

--- without.js
+++ without.js
@@ -22,6 +22,25 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const without = function(source, itemsToRemove") {
-  
-}
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  let match = false;
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        match = true;
+      }
+    }
+    if (!match) {
+      newArray.push(source[i]);
+    }
+    match = false;
+  }
+  return newArray;
+};
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
