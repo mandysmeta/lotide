a14b92682f295dd1c77c30010e5f15a04d3fe133
Miranda Miller <mirandaamiller13@gmail.com>
2022-01-15 03:05:57 +0000

    takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,8 +1,38 @@
-const takeUntil = function(array, callback) {
-  /*function returns "slice of array w/elements from beginning until callback returns a truthy value
-  (callback should only be one value-item in array)*/
-  newArray 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  } 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
 }
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+const takeUntil = function(array, callback) {
+  newArray = []
+  for (let i of array) {
+    if (!callback(i)) {
+      newArray.push(i)
+    } else {
+      break;
+    }
+  }
+  return newArray
+}
+
+const data = [1, 2, 3, 4, 5];
+const result = takeUntil(data, (x) => x === 4);
+console.log(result);
+
+/*compass ex:
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
@@ -11,4 +41,4 @@ console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+console.log(results2);*/
