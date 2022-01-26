e1aafd3df6514ee5e3ce1445c08d1de3bcc45b59
Miranda Miller <mirandaamiller13@gmail.com>
2022-01-26 16:01:55 +0000

    OG head.js

--- head.js
+++ head.js
@@ -1,8 +1,15 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = require('./assertEqual');
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 const head = function(input) {
   return (input[0]);
 }
-
-module.exports = head;
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
