a767acd7e4addb196661126b2b5a983bcb6db022
Miranda Miller <mirandaamiller13@gmail.com>
2022-01-26 16:05:24 +0000

    OG tail.js

--- tail.js
+++ tail.js
@@ -1,6 +1,11 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = require('../assertEqual');
-
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 const tail = function(list) {
   let answer = list.splice(1);
   return (answer);
@@ -9,5 +14,3 @@ const tail = function(list) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
-
-module.exports = tail
