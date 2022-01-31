7d57d78d1fab6008b421d12b0c898da5bb82ca27
Miranda Miller <mirandaamiller13@gmail.com>
2022-01-31 18:32:43 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -1,9 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
+  return true;
 };
 
 const assertArraysEqual = function(actual, expected) {
@@ -13,6 +17,7 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
 const middle = function(array) {
   let middleArray = [];
   if (array.length < 3) {
@@ -21,7 +26,7 @@ const middle = function(array) {
     middleArray.push(array[(array.length / 2) -1])
     middleArray.push(array[(array.length / 2)]);
   } else {
-    middleArray.push(array[(array.length) -1 / 2])
+    middleArray.push(array[(array.length -1) / 2])
   }
  return middleArray 
 }
--- without.js
+++ without.js
@@ -9,13 +9,13 @@ const eqArrays = function(arr1, arr2) {
   }
   return true
 }
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
 const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
