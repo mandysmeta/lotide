38f705cf291c58b1983644f5b9f2660388d93686
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-03 00:14:07 +0000

    assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,3 @@
-const inspect = require('util').inspect;
-
 const eqObjects = function(object1, object2) {
   // comparing lengths of object key arrays
   const objOneKeys = Object.keys(object1);
@@ -9,22 +7,16 @@ const eqObjects = function(object1, object2) {
   }
   // checks diff obj(object2) for all keys in main(object1)
   for (let key in object1) {
-    if (!object2.hasOwnProperty(key)) {
-      return false;
-    }
     // checks if value is an array on each
-    if (Array.isArray(object1[key])) {
-      if (Array.isArray(object2[key])) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
-          console.log('hello world');
+          //console.log('hello world');
           return false;
-        }
       }
-    } else {
+    } else if (object1[key] !== object2[key]) {
+
       //compares values if they are primitives
-      if (object1[key] !== object2[key]) {
         return false;
-      }
     }
   }
   return true;
@@ -32,10 +24,11 @@ const eqObjects = function(object1, object2) {
 
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
     console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
-console.log(assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1' }))
+assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1' })
