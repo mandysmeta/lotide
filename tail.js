6ccbf34d806387fe0ba41c93be287feb23d8a65f
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-16 19:11:12 +0000

    replaced assertEqual function with require

--- tail.js
+++ tail.js
@@ -1,11 +1,13 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// Remove FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
+
 const tail = function(list) {
   let answer = list.splice(1);
   return (answer);
