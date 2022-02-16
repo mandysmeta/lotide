17d35fddaa675a6bc4c441c91ac85c9d8f5925f0
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-16 23:12:16 +0000

    linted eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //goal: compare two arrays(arr1 & arr2)
 const eqArrays = function(arr1, arr2) {
   //if not the same - false
@@ -13,13 +5,15 @@ const eqArrays = function(arr1, arr2) {
     return false;
   } else {
     //if the same - true
-    if (arr1.length === arr2.length){
+    if (arr1.length === arr2.length) {
       return true;
     }
   }
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+module.exports = eqArrays;
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
