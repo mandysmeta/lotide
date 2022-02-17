6bec5506fa1a132f94423710a4cf1bc76bdbe017
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-17 00:53:53 +0000

    linted assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,14 @@
 const eqArrays = require('./eqArrays');
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
 //function goal: take in two arrays and log appropiate message
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
   }
+  console.log(`Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, "3"]);
 
 //no test code
