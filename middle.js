e89f9ea9194a70bdaa4604fdec344b59155d8e55
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-17 03:47:53 +0000

    refactored middle.js

--- middle.js
+++ middle.js
@@ -1,35 +1,23 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+//const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//function goal: get middle index, middle two if even array
 
 const middle = function(array) {
   let middleArray = [];
+  //if array is less than 3, return empty
   if (array.length < 3) {
-  return middleArray
+    return middleArray;
+    //if even, give middle two by dividing length in half 
   } else if (array.length % 2 === 0) {
-    middleArray.push(array[(array.length / 2) -1])
+    middleArray.push(array[(array.length / 2) - 1]);
     middleArray.push(array[(array.length / 2)]);
   } else {
-    middleArray.push(array[(array.length -1) / 2])
+    //or if odd
+    middleArray.push(array[(array.length - 1) / 2]);
   }
- return middleArray 
-}
-console.log(middle([1, 2])) //less than 3-empty
-console.log(middle([1, 2, 3, 4])) //even-2,3
-console.log(middle([1, 2, 3, 4, 5])) //odd-3
+  return middleArray;
+};
+assertArraysEqual((middle([1, 2])), []); //less than 3 -empty
+assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]); //even -2,3
+assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); //odd -3
