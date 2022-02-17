003472e46032f4a3d971199477b2ab334a0304ed
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-17 15:56:25 +0000

    updated headTest w/Mocha & Chai

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return console.log(`Assertion Passed: ${actual} === ${expected}`);
   }
-
+  
   console.log(`Assertion Failed: ${actual} !== ${expected}`);
 };
 
