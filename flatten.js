45d5081faed3568667e99ed77a665d40a0a57420
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-18 17:42:07 +0000

    linted flatten

--- flatten.js
+++ flatten.js
@@ -1,34 +1,19 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  } 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true
-};
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//const eqArrays = require('./eqArrays');
+//const assertArraysEqual = require('./assertArraysEqual');
+
 //take in array with elements-nested arrays, return flattened version
 const flatten = function(elements) {
   let flatArray = [];
   for (let element of elements) {
-    if (!Array.isArray(element)){
-      flatArray.push(element)
+    if (!Array.isArray(element)) {
+      flatArray.push(element);
     } else {
       for (let item of element) {
-        flatArray.push(item) 
+        flatArray.push(item);
       }
     }
   }
-  return flatArray 
+  return flatArray;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]))
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
