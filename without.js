f407d7bbfeac368f055ccffd33f1c86d3aa903b3
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-18 18:09:24 +0000

    linted without

--- without.js
+++ without.js
@@ -1,26 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  } 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true
-}
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const without = function(source, itemsToRemove) {
   let newArray = [];
@@ -38,8 +17,8 @@ const without = function(source, itemsToRemove) {
   }
   return newArray;
 };
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
