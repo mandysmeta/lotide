518a75e7db8a77372286f071a1114b73bd50314f
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-18 15:48:14 +0000

    refactored middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,22 @@
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual((middle([1, 2])), []); //less than 3 -empty
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]); //even -2,3
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); //odd -3
+describe("#middle", () => {
+  it("returns '[]' for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns '[2, 3]' for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns '[3]' for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+});  
+
+// assertArraysEqual((middle([1, 2])), []); //less than 3 -empty
+// assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]); //even -2,3
+// assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); //odd -3
