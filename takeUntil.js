a6e187bb67911836b3cce3c13deaaf2c6f7b89d4
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-18 17:53:20 +0000

    linted takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,36 +1,22 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  } 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true
-}
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+//const eqArrays = require('./eqArrays');
+//const assertArraysEqual = require('./assertArraysEqual');
+
 const takeUntil = function(array, callback) {
-  newArray = []
+  let newArray = [];
   for (let i of array) {
     if (!callback(i)) {
-      newArray.push(i)
+      newArray.push(i);
     } else {
       break;
     }
   }
-  return newArray
-}
+  return newArray;
+};
 
 const data = [1, 2, 3, 4, 5];
 const result = takeUntil(data, (x) => x === 4);
 console.log(result);
+
 
 /*compass ex:
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
