4d43534cdb595cc38a13cfbb6e32c11007052e56
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-18 15:39:50 +0000

    added test code to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,25 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const wordsTail = tail(["Yo Yo", "Lighthouse", "Labs"]);
-assertEqual(wordsTail.length, 2);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); //original array should return 3
+describe("#tail", () => {
+  it("returns 'Lighthouse', 'Labs' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns '2', for wordsTail.length", () => {
+    const wordsTail = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    assert.deepEqual(wordsTail.length, 2);
+  });
+
+  it("returns '3', for wordsTail.length", () => {
+     const words = ["Yo Yo", "Lighthouse", "Labs"];
+     tail(words);
+     assert.deepEqual(words.length, 3);
+   });
+});
+//const wordsTail = tail(["Yo Yo", "Lighthouse", "Labs"]);
+//assertEqual(wordsTail.length, 2);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3); //original array should return 3
