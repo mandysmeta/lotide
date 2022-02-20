2e7a2b3d8fb5255cd89e9441d7d563ff8e93a258
Miranda Miller <mirandaamiller13@gmail.com>
2022-02-20 22:55:58 +0000

    added export

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,30 +1,6 @@
-const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
 
-const eqObjects = function(object1, object2) {
-  // comparing lengths of object key arrays
-  const objOneKeys = Object.keys(object1);
-  const objTwoKeys = Object.keys(object2);
-  if (objOneKeys.length !== objTwoKeys.length) {
-    return false;
-  }
-  // checks diff obj(object2) for all keys in main(object1)
-  for (let key in object1) {
-    // checks if value is an array on each
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        //console.log('hello world');
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-
-      //compares values if they are primitives
-      return false;
-    }
-  }
-  return true;
-};
-
-
+//function goal: compare two objects, pass or fail if equal or not
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
@@ -33,4 +9,8 @@ const assertObjectsEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
+//console.log(`Example label: ${inspect(actual)}`);
+
 assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1' });
+
+module.exports = assertObjectsEqual;
--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+
 
 module.exports = {
   head,
   tail,
   middle,
   assertEqual,
-  eqArrays
+  eqArrays,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  
 };
